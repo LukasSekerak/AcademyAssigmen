2f7ec68fa14352c7d9454be073afdeb4ee46b920
LukasSekerak <lukas.sekerak@student.upjs.sk>
2023-04-11 14:09:25 +0200

    Last changes

--- src/test/java/sk/ness/academy/springbootHibernate/AssignmentApplicationTests.java
+++ src/test/java/sk/ness/academy/springbootHibernate/AssignmentApplicationTests.java
@@ -2,8 +2,11 @@ package sk.ness.academy.springbootHibernate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import sk.ness.academy.config.TestDataSourceConfig;
 
 @SpringBootTest
+@ContextConfiguration(classes = { TestDataSourceConfig.class })
 class AssignmentApplicationTests {
 
 	@Test
